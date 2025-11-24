91e3cfc32ac77468ac73680dc48fffd4eb92ba27
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-24 17:22:08 +0100

    add OrderRequest and OrderResponse classes for order processing

---
+++ src/main/java/org/example/smartshop/dto/order/OrderRequest.java
@@ -0,0 +1,20 @@
+package org.example.smartshop.dto.order;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class OrderRequest {
+
+    @NotNull(message = "L'ID du client est requis")
+    private Long clientId;
+
+    @NotEmpty(message = "La commande doit contenir au moins un article")
+    private List<OrderItemRequestDTO> items;
+
+    // Validation Regex stricte exigée par le brief
+    @Pattern(regexp = "PROMO-[A-Z0-9]{4}", message = "Format code promo invalide (ex: PROMO-1234)")
+    private String codePromo;
+}
---
+++ src/main/java/org/example/smartshop/dto/order/OrderResponse.java
@@ -0,0 +1,23 @@
+package org.example.smartshop.dto.order;
+
+import org.example.smartshop.enums.OrderStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class OrderResponse {
+    private Long id;
+    private Long clientId;
+    private String clientName; // Utile pour l'affichage
+    private LocalDateTime date;
+    private OrderStatus status;
+
+    // Détails financiers calculés
+    private BigDecimal sousTotalHT;
+    private BigDecimal montantRemise; // Fidélité + Promo
+    private BigDecimal montantTVA;
+    private BigDecimal totalTTC;
+    private BigDecimal montantRestant; // Important pour savoir si on peut valider
+
+    private List<OrderItemResponseDTO> items;
+}
