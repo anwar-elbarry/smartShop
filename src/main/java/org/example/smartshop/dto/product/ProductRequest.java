fffa1bc6c0c0a15a804a46804cb2b6ec38ba3b87
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-25 15:17:35 +0100

    add Lombok @Data annotation to ProductRequest class for boilerplate reduction

--- src/main/java/org/example/smartshop/dto/product/ProductRequest.java
+++ src/main/java/org/example/smartshop/dto/product/ProductRequest.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
+@Data
 public class ProductRequest {
     @NotBlank(message = "Le nom du produit est requis")
     private String nom;
