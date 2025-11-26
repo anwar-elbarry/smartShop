c9bd9358c838ef10ff429c9da5e03a7bd365a5c6
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-26 10:57:54 +0100

    add register method to AuthService for user registration

--- src/main/java/org/example/smartshop/service/Auth/AuthService.java
+++ src/main/java/org/example/smartshop/service/Auth/AuthService.java
@@ -2,6 +2,7 @@ package org.example.smartshop.service.Auth;
 
 import lombok.RequiredArgsConstructor;
 import org.example.smartshop.dto.auth.LoginRequest;
+import org.example.smartshop.dto.auth.RegisterRequest;
 import org.example.smartshop.entity.User;
 import org.example.smartshop.repository.UserRepository;
 import org.mindrot.jbcrypt.BCrypt;
@@ -19,5 +20,17 @@ public class AuthService {
             throw new RuntimeException("Mot de passe incorrect");
         }
         return user;
+    }
+
+    public User register(RegisterRequest request){
+        String passwordHash = BCrypt.hashpw(request.getPassword(),BCrypt.gensalt());
+        User user = User.builder()
+                .username(request.getUsername())
+                .role(request.getRole())
+                .password(passwordHash)
+                .build();
+        userRepository.save(user);
+        return user;
+
     }
 }
