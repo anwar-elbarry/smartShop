89b774c0428b1e481953267c9b52d234e0c3a828
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-26 09:45:55 +0100

    add AuthService with login functionality and password verification

---
+++ src/main/java/org/example/smartshop/service/Auth/AuthService.java
@@ -0,0 +1,23 @@
+package org.example.smartshop.service.Auth;
+
+import lombok.RequiredArgsConstructor;
+import org.example.smartshop.dto.auth.LoginRequest;
+import org.example.smartshop.entity.User;
+import org.example.smartshop.repository.UserRepository;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    public User login(LoginRequest request){
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(()-> new RuntimeException("user not found") );
+
+        if (!BCrypt.checkpw(request.getPassword(),user.getPassword())){
+            throw new RuntimeException("Mot de passe incorrect");
+        }
+        return user;
+    }
+}
