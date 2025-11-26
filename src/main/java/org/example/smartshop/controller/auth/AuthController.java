06af6115f40a3885754b2200c13fda6695d0066f
anouar eil barry <elbarryanwar37@gmail.com>
2025-11-26 09:45:40 +0100

    add AuthController with login endpoint and session management

---
+++ src/main/java/org/example/smartshop/controller/auth/AuthController.java
@@ -0,0 +1,58 @@
+package org.example.smartshop.controller.auth;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.example.smartshop.dto.auth.LoginRequest;
+import org.example.smartshop.dto.auth.LoginResponse;
+import org.example.smartshop.entity.User;
+import org.example.smartshop.service.Auth.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Tag(name = "Auth",description = "Auth management APIs")
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+
+    @Operation(summary = "user login")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "the user logged successfully"),
+            @ApiResponse(responseCode = "400", description = "Validation error")
+    })
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Validated @RequestBody LoginRequest request , HttpSession session){
+        try {
+        User user = authService.login(request);
+        session.setAttribute("UserId",user.getId());
+        session.setAttribute("role",user.getRole().name());
+
+        LoginResponse loginResponse = LoginResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .role(user.getRole())
+                .build();
+
+            return ResponseEntity.ok(loginResponse);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of(
+                            "error", "Authentication failed",
+                            "message" , e.getMessage()
+                    ));
+        }
+    }
+}
