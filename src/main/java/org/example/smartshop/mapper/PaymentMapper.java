cd7a7e2102fb8cf2ac9b68082f187136f1a13f72
anouar eil barry <elbarryanwar37@gmail.com>
2025-12-08 12:06:55 +0100

    update PaymentMapper to ignore specific fields during mapping

--- src/main/java/org/example/smartshop/mapper/PaymentMapper.java
+++ src/main/java/org/example/smartshop/mapper/PaymentMapper.java
@@ -2,18 +2,21 @@ package org.example.smartshop.mapper;
 
 import org.example.smartshop.dto.payment.PaymentRequest;
 import org.example.smartshop.dto.payment.PaymentResponse;
+
 import org.example.smartshop.entity.Payment;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface PaymentMapper {
-    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "datePaiement", ignore = true)
     @Mapping(target = "order.id", source = "orderId")
     Payment toEntity(PaymentRequest paymentRequest);
 
+    @Mapping(target = "orderId", source = "order.id")
     PaymentResponse toResponse(Payment entity);
 }
-
