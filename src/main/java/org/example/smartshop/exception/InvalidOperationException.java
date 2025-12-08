387a66f86a99de24b79befe5bb005ae364c17888
anouar eil barry <elbarryanwar37@gmail.com>
2025-12-08 12:07:58 +0100

    add InvalidOperationException class for handling specific runtime exceptions

---
+++ src/main/java/org/example/smartshop/exception/InvalidOperationException.java
@@ -0,0 +1,15 @@
+package org.example.smartshop.exception;
+
+public class InvalidOperationException extends RuntimeException {
+    public InvalidOperationException(String message) {
+    super(message);
+}
+
+    public InvalidOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidOperationException(Throwable cause) {
+        super(cause);
+    }
+}
