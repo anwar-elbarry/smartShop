3a2ccebd68a5bd249a7fd5fc3f847c7d5f6eee4e
anouar eil barry <elbarryanwar37@gmail.com>
2025-12-09 07:35:30 +0100

    remove unused constructors from InvalidOperationException

--- src/main/java/org/example/smartshop/exception/InvalidOperationException.java
+++ src/main/java/org/example/smartshop/exception/InvalidOperationException.java
@@ -4,12 +4,4 @@ public class InvalidOperationException extends RuntimeException {
     public InvalidOperationException(String message) {
     super(message);
 }
-
-    public InvalidOperationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InvalidOperationException(Throwable cause) {
-        super(cause);
-    }
 }
